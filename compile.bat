2a98565c7625ce796843b52c399f73864f352cb3
Caleb A. Collar <49846301+Mindstormer-0@users.noreply.github.com>
2021-12-13 22:39:33 -0600

    Removed build batch tool
    os specific

--- compile.bat
+++
@@ -1,7 +0,0 @@
-if not "%minimized%"=="" goto :minimized
-set minimized=true
-start /min cmd /C "%~dpnx0"
-goto :EOF
-:minimized
-del /S src\*.class
-javac -cp "lib\*" src\*.java
--- launch.bat
+++
@@ -1,7 +0,0 @@
-if not "%minimized%"=="" goto :minimized
-set minimized=true
-start /min cmd /C "%~dpnx0"
-goto :EOF
-:minimized
-start /min cmd /C "%~dpnx0 & java -cp "src;lib\*" Server"
-start /min cmd /C "%~dpnx0 & java -cp "src;lib\*" Driver"
